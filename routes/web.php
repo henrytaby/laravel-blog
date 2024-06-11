90e346d9df8997d0046f0aa93ee82d2dc41b0d59
Henry Taby <henry.taby@gmail.com>
2024-06-11 00:46:07 -0400

    autentificación

--- routes/web.php
+++ routes/web.php
@@ -1,27 +1,24 @@
 <?php
-use Illuminate\Http\Request;
+
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
-/**
- * Route::get       | Consultar
- * Route::post      | Guardar
- * Route::delete    | Eliminar
- * Route::put       | Actualizar
- */
-/*
-Route::get('/',[PageController::class,'home'])->name('home');
-Route::get('blog', [PageController::class,'blog'])->name('blog');
-Route::get('blog/{slug}', [PageController::class,'post'])->name('post');
-*/
+
 Route::controller(PageController::class)->group(function (){
     Route::get('/','home')->name('home');
     Route::get('blog', 'blog')->name('blog');
     Route::get('blog/{post:slug}', 'post')->name('post');
 });
 
-/**
- * con request!
- */
-Route::get('/buscar', function (Request $request) {
-    return $request->all();
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
