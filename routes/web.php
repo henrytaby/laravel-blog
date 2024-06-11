618e8e57523ef12d276d71e606ab1e447573d212
Henry Taby <henry.taby@gmail.com>
2024-06-11 11:41:03 -0400

    modulo de posts en dashboard

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
 
 Route::controller(PageController::class)->group(function (){
     Route::get('/','home')->name('home');
@@ -20,5 +21,8 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('posts', PostController::class);
+
 
 require __DIR__.'/auth.php';
